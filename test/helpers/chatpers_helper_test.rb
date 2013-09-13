a035c0a5c3d073afd1e33d2dc6eaf08749c529d8
Akira Panda <p.chenliang@gmail.com>
2013-09-13 10:52:36 +0800

    deleted

--- test/helpers/chatpers_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ChatpersHelperTest < ActionView::TestCase
-end
--- test/models/chatper_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ChatperTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
