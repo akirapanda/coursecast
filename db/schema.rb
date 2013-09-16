bbbc8fee8e2bb6a77f102823d4074fbaf1b372d4
Akira Panda <p.chenliang@gmail.com>
2013-09-16 23:30:06 +0800

    add an adminfield into user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130915135104) do
+ActiveRecord::Schema.define(version: 20130916152828) do
 
   create_table "chapters", force: true do |t|
     t.string   "title"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20130915135104) do
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
